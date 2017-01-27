4ae895b2361376b5c3da58551781582e4360ae42
brad winter <braddjwinter@gmail.com>
2017-01-27 18:26:46 +0800

    Fix

--- keystone.js
+++ keystone.js
@@ -39,7 +39,6 @@ keystone.init({
 
 // Load your project's Models
 keystone.import('models');
-
 // Setup common locals for your templates. The following are required for the
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
@@ -47,3 +46,25 @@ keystone.set('locals', {
 	_: require('lodash'),
 	env: keystone.get('env'),
 	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+
+// Load your project's Routes
+keystone.set('routes', require('./routes'));
+
+// Switch Keystone Email defaults to handlebars
+keystone.Email.defaults.templateExt = 'hbs';
+keystone.Email.defaults.templateEngine = require('handlebars');
+
+
+// Configure the navigation bar in Keystone's Admin UI
+keystone.set('nav', {
+		
+	posts: ['posts', 'post-categories'],
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+// Start Keystone to connect to your database and initialise the web server
+
+keystone.start();
