4e80080b0a30bf4b3a9e42ddb4841daacc6595cf
Brad-ipad <braddjwinter@gmail.com>
2017-01-27 16:19:11 +0800

    Content.js

---
+++ models/Content.js
@@ -0,0 +1,18 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * User Model
+ * ==========
+ */
+var Content = new keystone.List('Content');
+
+Content.add({
+	name: { type: Types.String, required: true, index: true, unique: true },
+	content: { type: Types.Markdown, initial: true, required: true}
+});
+
+
+Content.register();
+
+
